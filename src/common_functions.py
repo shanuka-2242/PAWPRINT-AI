fe4656e358dfc59285278ca6b25e84ca6bd0e512
shanuka_2242 <shanukapayoe@gmail.com>
2024-10-18 22:35:39 +0530

    2024.10.18 changes

--- src/common_functions.py
+++ src/common_functions.py
@@ -1,26 +1,23 @@
 import sqlite3
-from models.Owner import Owner 
 
 # Function to get an owner by NIC from the SQLite database
 def get_owner_by_nic(nic: str):
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
-    
     # SQL query to select the owner based on NIC
-    cursor.execute("SELECT nic, full_name, phone, email, current_address, password FROM tb_owners WHERE nic = ?", (nic,))
+    cursor.execute('SELECT nic, full_name, phone, email, current_address, password FROM tb_owners WHERE nic = ?', (nic,))
     owner = cursor.fetchone()
     conn.close()
-    
     # Return the owner if found, otherwise None
     if owner:
         return {
-            "NIC": owner[0],
-            "FullName": owner[1],
-            "Phone": owner[2],
-            "Email": owner[3],
-            "CurrentAddress": owner[4],
-            "Password": owner[5],
+            'NIC': owner[0],
+            'FullName': owner[1],
+            'Phone': owner[2],
+            'Email': owner[3],
+            'CurrentAddress': owner[4],
+            'Password': owner[5],
         }
     else:
         return None
@@ -30,23 +27,21 @@ def get_owners():
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
-    
     # SQL query to select the owner based on NIC
-    cursor.execute("SELECT * FROM tb_owners")
+    cursor.execute('SELECT * FROM tb_owners')
     owners_list = cursor.fetchall()
     conn.close()
-    
     # Return the owner if found, otherwise None
     if owners_list:
         owners_data = []
         for owner in owners_list:
             owner_data = {
-                "NIC": owner[0],
-                "FullName": owner[1],
-                "Phone": owner[2],
-                "Email": owner[3],
-                "CurrentAddress": owner[4],
-                "Password": owner[5]
+                'NIC': owner[0],
+                'FullName': owner[1],
+                'Phone': owner[2],
+                'Email': owner[3],
+                'CurrentAddress': owner[4],
+                'Password': owner[5]
                 }
             owners_data.append(owner_data)
         return owners_data
@@ -58,34 +53,29 @@ def put_owner(nic:str, full_name:str, phone:str, email:str, current_address:str,
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
-    
     # SQL query to insert registered dog into the SQLite database
-    cursor.execute("INSERT INTO tb_owners (nic, full_name, phone, email, current_address, password) VALUES (?, ?, ?, ?, ?, ?)", (nic, full_name, phone, email, current_address, password))
+    cursor.execute('INSERT INTO tb_owners (nic, full_name, phone, email, current_address, password) VALUES (?, ?, ?, ?, ?, ?)', (nic, full_name, phone, email, current_address, password))
     conn.commit()
     # Verification step: Retrieve the inserted record to confirm insertion
-    cursor.execute("SELECT * FROM tb_owners WHERE nic = ?", (nic,))
-
+    cursor.execute('SELECT * FROM tb_owners WHERE nic = ?', (nic,))
      # Fetch the result of the query
-    inserted_dog = cursor.fetchone()
+    inserted_owner = cursor.fetchone()
     conn.close()
-    
     # Return the owner if found, otherwise None
-    if inserted_dog:
-        return inserted_dog
+    if inserted_owner:
+        return True
     else:
-        return None 
+        return False 
     
 # Function to get registred dogs list from the SQLite database
 def get_registered_dogs():
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
-    
     # SQL query to select the owner based on NIC
-    cursor.execute("SELECT * FROM tb_registered_dogs")
+    cursor.execute('SELECT * FROM tb_registered_dogs')
     registered_dog_list = cursor.fetchall()
     conn.close()
-    
     # Return the owner if found, otherwise None
     if registered_dog_list:
         return registered_dog_list
@@ -97,19 +87,44 @@ def put_register_dog(dog_name:str, breed:str, age:str, feature_vector:bytes, own
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
-    
     # SQL query to insert registered dog into the SQLite database
-    cursor.execute("INSERT INTO tb_registered_dogs (dog_name, breed, age, feature_vector, owner_nic) VALUES (?, ?, ?, ?, ?)", (dog_name, breed, age, feature_vector, owner_nic))
+    cursor.execute('INSERT INTO tb_registered_dogs (dog_name, breed, age, feature_vector, owner_nic) VALUES (?, ?, ?, ?, ?)', (dog_name, breed, age, feature_vector, owner_nic))
     conn.commit()
     # Verification step: Retrieve the inserted record to confirm insertion
-    cursor.execute("SELECT * FROM tb_registered_dogs WHERE dog_name = ? AND owner_nic = ?", (dog_name, owner_nic))
-
+    cursor.execute('SELECT * FROM tb_registered_dogs WHERE dog_name = ? AND owner_nic = ?', (dog_name, owner_nic))
      # Fetch the result of the query
     inserted_dog = cursor.fetchone()
     conn.close()
-    
     # Return the owner if found, otherwise None
     if inserted_dog:
-        return inserted_dog
+        return True
+    else:
+        return False 
+    
+# Function to get registred dog by entry ID & return dog info and owner info from the SQLite database
+def get_registered_dog_and_owner(entry_id:str):
+    # Connect to the SQLite database
+    conn = sqlite3.connect('Database/pawprint.db')
+    cursor = conn.cursor()
+    # Verification step: Retrieve the inserted record to confirm insertion
+    cursor.execute('SELECT * FROM tb_registered_dogs WHERE id = ?', (entry_id,))
+     # Fetch the result of the query
+    registered_dog_info = cursor.fetchone()
+    conn.close()
+    # Return the owner if found, otherwise None
+    if registered_dog_info:
+        owner_info = get_owner_by_nic(registered_dog_info[5])
+        if owner_info:
+            verifiedData = {
+                'DogName': registered_dog_info[1],
+                'Breed': registered_dog_info[2],
+                'Age': registered_dog_info[3],
+                'NIC': owner_info['NIC'],
+                'FullName': owner_info['FullName'],
+                'Phone': owner_info['Phone'],
+                'Email': owner_info['Email'],
+                'CurrentAddress': owner_info['CurrentAddress'],
+                }
+        return verifiedData
     else:
         return None 
--- src/main.py
+++ src/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI,UploadFile,File,Form,HTTPException
+from fastapi import FastAPI,UploadFile,File,Form,HTTPException,Response
 import uvicorn
 import shutil
 import tempfile
@@ -11,15 +11,18 @@ from common_functions import get_owners
 from common_functions import put_owner
 from common_functions import get_registered_dogs
 from common_functions import put_register_dog
+from common_functions import get_registered_dog_and_owner
 from models.Owner import Owner 
 import os
 
 app = FastAPI()
 
+
 # Main endpoint
 @app.get('/')
 async def welcome():
     return 'Welcome To PAWPRINT-AI Backend'
+
 
 # Endpoint for get signedin owners list
 @app.get('/registered_owners')
@@ -28,7 +31,8 @@ async def registered_owners():
     if owners_list:
         return owners_list
     else:
-        raise HTTPException(status_code=404, detail="Owners not found")
+        raise HTTPException(status_code=404)
+
 
 # Endpoint for get owner by NIC
 @app.get('/registered_owners/{nic}', response_model=Owner)
@@ -37,8 +41,9 @@ async def registered_owner(nic: str):
     if owner:
         return owner
     else:
-        raise HTTPException(status_code=404, detail="Owner not found")
+        raise HTTPException(status_code=404)
     
+
 # Endpoint for register owner
 @app.post('/register_owner')
 async def register_owner(nic:str = Form(...), 
@@ -47,12 +52,12 @@ async def register_owner(nic:str = Form(...),
                         email: str = Form(...),
                         current_address: str = Form(...),
                         password: str = Form(...)):
-    
     owner = put_owner(nic, full_name, phone, email, current_address, password)
     if owner:
-        return owner
+        return Response(status_code=200)
     else:
-        raise HTTPException(status_code=404, detail="Owner not found")
+        raise HTTPException(status_code=404)
+
 
 # Endpoint for Register dog 
 @app.post('/register-dog')
@@ -61,96 +66,86 @@ async def register_dog(file: UploadFile = File(...),
                        breed:str = Form(...), 
                        age:str = Form(...), 
                        owner_nic: str = Form(...)):
-
     # Create a temporary directory
     temp_dir = tempfile.mkdtemp()
     # Create an empty dictionary to store the retrieved data
     retrieved_dataset = {}
     # Save the uploaded file
     image_file_path = os.path.join(temp_dir, file.filename)
-    with open(image_file_path, "wb") as f:
+    with open(image_file_path, 'wb') as f:
         shutil.copyfileobj(file.file, f)
-
     # Fetch all the rows returned by the query
     registered_dogs_list = get_registered_dogs()
-
     # Iterate over the rows and populate the retrieved_dataset dictionary
     if not registered_dogs_list:
         owner_inputed_image_feature_array = extract_features(image_file_path)
         owner_inputed_image_feature_array_blob = sqlite3.Binary(owner_inputed_image_feature_array.tobytes())
         inserted_dog = put_register_dog(dog_name, breed, age, owner_inputed_image_feature_array_blob, owner_nic)
         if inserted_dog:
-            return {"message": f"Dog '{dog_name}' registered successfully"}
+            return Response(status_code=200)
         else:
-            raise HTTPException(status_code=500, detail="Dog couldn't registered.")
+            raise HTTPException(status_code=500)
     else:
         array_present = False
-
         for registered_dog in registered_dogs_list:
             entry_id = registered_dog[0]
             feature_array_buffer = registered_dog[4]
             feature_array = np.frombuffer(feature_array_buffer, dtype=np.float32)
             retrieved_dataset[entry_id] = feature_array
-            
-        print(len(list(retrieved_dataset.values())))
         owner_inputed_image_feature_array = extract_features(image_file_path)
-
         for feature_array in retrieved_dataset.values():
             if np.array_equal(feature_array, owner_inputed_image_feature_array):
                 array_present = True
                 break
-
         # If the array is not present, add it to the dataset
         if not array_present:
             # Convert the target_array to a binary representation
             owner_inputed_image_feature_array_blob = sqlite3.Binary(owner_inputed_image_feature_array.tobytes())
             inserted_dog = put_register_dog(dog_name, breed, age, owner_inputed_image_feature_array_blob, owner_nic)
             if inserted_dog:
-                return {"message": f"Dog '{dog_name}' registered successfully"}
+                return Response(status_code=200)
             else:
-                 raise HTTPException(status_code=500, detail="Dog couldn't registered.")
+                 raise HTTPException(status_code=500)
         else:
-            return f"'{dog_name}' is already registered."
+            return Response(content='Dog Already Registered By An Owner', status_code=200)
 
+
+# Endpoint for verify ownership 
 @app.post('/verify-ownership')
 async def verify_ownership(file: UploadFile = File(...)):
     # Create a temporary directory
     temp_dir = tempfile.mkdtemp()
     # Save the uploaded file
     image_file_path = os.path.join(temp_dir, file.filename)
-
-    with open(image_file_path, "wb") as f:
+    with open(image_file_path, 'wb') as f:
         shutil.copyfileobj(file.file, f)
-    
     registered_dogs_list = get_registered_dogs()
-
     if registered_dogs_list:
         # Create an empty dictionary to store the retrieved data
         retrieved_dataset = {}
-
         # Iterate over the rows and populate the retrieved_dataset dictionary
         for registered_dog in registered_dogs_list:
-            entry_id = registered_dog[5]
+            entry_id = registered_dog[0]
             feature_array_buffer = registered_dog[4]
             feature_array = np.frombuffer(feature_array_buffer, dtype=np.float32)
             retrieved_dataset[entry_id] = feature_array
-
         # Extract features from the new image
         user_inputed_image_features = extract_features(image_file_path)
-
         # Compare the new image features with the dataset using cosine similarity
         max_similarity = -1
         max_similarity_entry_id = None
- 
         for entry_id, existing_img_features in retrieved_dataset.items():
             similarity = 1 - cosine(existing_img_features, user_inputed_image_features)
             if similarity > max_similarity:
                 max_similarity = similarity
                 max_similarity_entry_id = entry_id
-
-        return {'Most Similar Entry ID': max_similarity_entry_id}
+        truncated_number = float(str(max_similarity)[:3])
+        if truncated_number > 0.8:
+            return get_registered_dog_and_owner(max_similarity_entry_id)
+        else:
+            raise HTTPException(status_code=404)
     else:
-        return f"No Dogs Registered Into The Paw Print Yet."
+        return HTTPException(status_code=404)
     
 if __name__ == '__main__':
     uvicorn.run(app)
