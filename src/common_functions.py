8a195bf9de5c5042340bc7965e48b26b1e23a37c
shanuka_2242 <shanukapayoe@gmail.com>
2024-10-26 04:07:25 +0530

    chnages 2024/10/26

--- src/common_functions.py
+++ src/common_functions.py
@@ -2,13 +2,16 @@ import sqlite3
 
 # Function to get an owner by NIC from the SQLite database
 def get_owner_by_nic(nic: str):
+
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
+
     # SQL query to select the owner based on NIC
     cursor.execute('SELECT nic, full_name, phone, email, current_address, password FROM tb_owners WHERE nic = ?', (nic,))
     owner = cursor.fetchone()
     conn.close()
+
     # Return the owner if found, otherwise None
     if owner:
         return {
@@ -24,13 +27,16 @@ def get_owner_by_nic(nic: str):
     
 # Function to get owners list from the SQLite database
 def get_owners():
+
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
+
     # SQL query to select the owner based on NIC
     cursor.execute('SELECT * FROM tb_owners')
     owners_list = cursor.fetchall()
     conn.close()
+
     # Return the owner if found, otherwise None
     if owners_list:
         owners_data = []
@@ -50,17 +56,22 @@ def get_owners():
     
 # Function to add owner into the SQLite database
 def put_owner(nic:str, full_name:str, phone:str, email:str, current_address:str, password:str):
+
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
+
     # SQL query to insert registered dog into the SQLite database
     cursor.execute('INSERT INTO tb_owners (nic, full_name, phone, email, current_address, password) VALUES (?, ?, ?, ?, ?, ?)', (nic, full_name, phone, email, current_address, password))
     conn.commit()
+
     # Verification step: Retrieve the inserted record to confirm insertion
     cursor.execute('SELECT * FROM tb_owners WHERE nic = ?', (nic,))
+
      # Fetch the result of the query
     inserted_owner = cursor.fetchone()
     conn.close()
+
     # Return the owner if found, otherwise None
     if inserted_owner:
         return True
@@ -69,13 +80,16 @@ def put_owner(nic:str, full_name:str, phone:str, email:str, current_address:str,
     
 # Function to get registred dogs list from the SQLite database
 def get_registered_dogs():
+
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
+
     # SQL query to select the owner based on NIC
     cursor.execute('SELECT * FROM tb_registered_dogs')
     registered_dog_list = cursor.fetchall()
     conn.close()
+
     # Return the owner if found, otherwise None
     if registered_dog_list:
         return registered_dog_list
@@ -83,18 +97,23 @@ def get_registered_dogs():
         return None
 
 # Function to add registred dog into the SQLite database
-def put_register_dog(dog_name:str, breed:str, age:str, feature_vector:bytes, owner_nic:str):
+def put_register_dog(dog_name:str, breed:str, age:str, feature_vector:bytes, owner_nic:str, dog_image:bytes):
+
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
+
     # SQL query to insert registered dog into the SQLite database
-    cursor.execute('INSERT INTO tb_registered_dogs (dog_name, breed, age, feature_vector, owner_nic) VALUES (?, ?, ?, ?, ?)', (dog_name, breed, age, feature_vector, owner_nic))
+    cursor.execute('INSERT INTO tb_registered_dogs (dog_name, breed, age, feature_vector, owner_nic, dog_image) VALUES (?, ?, ?, ?, ?, ?)', (dog_name, breed, age, feature_vector, owner_nic, dog_image))
     conn.commit()
+
     # Verification step: Retrieve the inserted record to confirm insertion
     cursor.execute('SELECT * FROM tb_registered_dogs WHERE dog_name = ? AND owner_nic = ?', (dog_name, owner_nic))
+
      # Fetch the result of the query
     inserted_dog = cursor.fetchone()
     conn.close()
+
     # Return the owner if found, otherwise None
     if inserted_dog:
         return True
@@ -103,28 +122,56 @@ def put_register_dog(dog_name:str, breed:str, age:str, feature_vector:bytes, own
     
 # Function to get registred dog by entry ID & return dog info and owner info from the SQLite database
 def get_registered_dog_and_owner(entry_id:str):
+
     # Connect to the SQLite database
     conn = sqlite3.connect('Database/pawprint.db')
     cursor = conn.cursor()
+
     # Verification step: Retrieve the inserted record to confirm insertion
     cursor.execute('SELECT * FROM tb_registered_dogs WHERE id = ?', (entry_id,))
+
      # Fetch the result of the query
     registered_dog_info = cursor.fetchone()
     conn.close()
+
     # Return the owner if found, otherwise None
     if registered_dog_info:
         owner_info = get_owner_by_nic(registered_dog_info[5])
         if owner_info:
             verifiedData = {
-                'DogName': registered_dog_info[1],
-                'Breed': registered_dog_info[2],
-                'Age': registered_dog_info[3],
-                'NIC': owner_info['NIC'],
-                'FullName': owner_info['FullName'],
-                'Phone': owner_info['Phone'],
-                'Email': owner_info['Email'],
-                'CurrentAddress': owner_info['CurrentAddress'],
+                    'Dog':{
+                        'EntryID': registered_dog_info[0],
+                        'Name': registered_dog_info[1],
+                        'Breed': registered_dog_info[2],
+                        'Age': registered_dog_info[3]
+                    },
+                    'Owner':{
+                        'NIC': owner_info['NIC'],
+                        'FullName': owner_info['FullName'],
+                        'Phone': owner_info['Phone'],
+                        'Email': owner_info['Email'],
+                        'CurrentAddress': owner_info['CurrentAddress']
+                    }
                 }
         return verifiedData
     else:
         return None 
+    
+# Function to get dog image from the database
+def get_dog_img_by_entry_id(entry_id: int):
+
+    # Connect to the SQLite database
+    conn = sqlite3.connect('Database/pawprint.db')
+    cursor = conn.cursor()
+    
+    # Fetch image data based on the dog's ID
+    cursor.execute("SELECT dog_image FROM tb_registered_dogs WHERE id = ?", (entry_id,))
+    dog_image = cursor.fetchone()
+    
+    conn.close()
+
+    if dog_image:
+        return dog_image[0]
+    
+    else:
+        return None
--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI,UploadFile,File,Form,HTTPException,Response
+from fastapi.responses import StreamingResponse
 import uvicorn
 import shutil
 import tempfile
@@ -12,7 +13,10 @@ from common_functions import put_owner
 from common_functions import get_registered_dogs
 from common_functions import put_register_dog
 from common_functions import get_registered_dog_and_owner
+from common_functions import get_dog_img_by_entry_id
 import os
+import io
+
 
 app = FastAPI()
 
@@ -60,89 +64,170 @@ async def register_owner(nic:str = Form(...),
 
 # Endpoint for Register dog 
 @app.post('/register_dog')
-async def register_dog(file: UploadFile = File(...), 
+async def register_dog(nose_image: UploadFile = File(...), 
+                       dog_image: UploadFile = File(...), 
                        dog_name:str = Form(...), 
                        breed:str = Form(...), 
                        age:str = Form(...), 
                        owner_nic: str = Form(...)):
+    
     # Create a temporary directory
     temp_dir = tempfile.mkdtemp()
+
     # Create an empty dictionary to store the retrieved data
     retrieved_dataset = {}
-    # Save the uploaded file
-    image_file_path = os.path.join(temp_dir, file.filename)
-    with open(image_file_path, 'wb') as f:
-        shutil.copyfileobj(file.file, f)
-    # Fetch all the rows returned by the query
+
+    # File path to save the uploaded files in the temp folders
+    nose_image_file_path = os.path.join(temp_dir, nose_image.filename)
+    dog_image_file_path = os.path.join(temp_dir, dog_image.filename)
+
+    # Save dog nose image into temp folder path
+    with open(nose_image_file_path, 'wb') as f:
+        shutil.copyfileobj(nose_image.file, f)
+
+    # Save dog image into temp folder path
+    with open(dog_image_file_path, 'wb') as f:
+        shutil.copyfileobj(dog_image.file, f)
+
+    # Fetch all the registered dogs
     registered_dogs_list = get_registered_dogs()
-    # Iterate over the rows and populate the retrieved_dataset dictionary
+
+    # If the fetched registered dog is empty save the dog into the database
     if not registered_dogs_list:
-        owner_inputed_image_feature_array = extract_features(image_file_path)
+
+        # Read the dog image as bytes to store in the database
+        with open(dog_image_file_path, 'rb') as f:
+            dog_image_as_bytes = f.read()
+
+        # Extract feature from the image and convert feature array into BLOB format to save in the database
+        owner_inputed_image_feature_array = extract_features(nose_image_file_path)
         owner_inputed_image_feature_array_blob = sqlite3.Binary(owner_inputed_image_feature_array.tobytes())
-        inserted_dog = put_register_dog(dog_name, breed, age, owner_inputed_image_feature_array_blob, owner_nic)
+
+        #Insert the registered dog into the database
+        inserted_dog = put_register_dog(dog_name, breed, age, owner_inputed_image_feature_array_blob, owner_nic, dog_image_as_bytes)
+
         if inserted_dog:
             return Response(status_code=200)
         else:
             raise HTTPException(status_code=500)
+    
+    # If the fetched registered dog is not empty
     else:
+
+        # Maintaining Bool to check if feature array is already present
         array_present = False
+
+        # Iterate trough fetched registered dog list and populate dictionary
         for registered_dog in registered_dogs_list:
             entry_id = registered_dog[0]
             feature_array_buffer = registered_dog[4]
             feature_array = np.frombuffer(feature_array_buffer, dtype=np.float32)
             retrieved_dataset[entry_id] = feature_array
-        owner_inputed_image_feature_array = extract_features(image_file_path)
+
+        # Extract feature from the image 
+        owner_inputed_image_feature_array = extract_features(nose_image_file_path)
+
+        # Check if array is alrady in the database
         for feature_array in retrieved_dataset.values():
             if np.array_equal(feature_array, owner_inputed_image_feature_array):
                 array_present = True
                 break
+
         # If the array is not present, add it to the dataset
         if not array_present:
+
             # Convert the target_array to a binary representation
             owner_inputed_image_feature_array_blob = sqlite3.Binary(owner_inputed_image_feature_array.tobytes())
-            inserted_dog = put_register_dog(dog_name, breed, age, owner_inputed_image_feature_array_blob, owner_nic)
+
+            # Read the dog image as bytes to store in the database
+            with open(dog_image_file_path, 'rb') as f:
+                dog_image_as_bytes = f.read()
+
+            # Insert dog into the database
+            inserted_dog = put_register_dog(dog_name, breed, age, owner_inputed_image_feature_array_blob, owner_nic, dog_image_as_bytes)
+
+            # Check if inserted dog is true of false and return status code
             if inserted_dog:
                 return Response(status_code=200)
             else:
                  raise HTTPException(status_code=500)
         else:
-            return Response(content='Dog Already Registered By An Owner', status_code=200)
+            return Response(status_code=400)
 
 
 # Endpoint for verify ownership 
 @app.post('/verify_ownership')
 async def verify_ownership(file: UploadFile = File(...)):
+
     # Create a temporary directory
     temp_dir = tempfile.mkdtemp()
-    # Save the uploaded file
+
+    # Tempory file path to save the uploaded nose image
     image_file_path = os.path.join(temp_dir, file.filename)
+
+    # Save the image
     with open(image_file_path, 'wb') as f:
         shutil.copyfileobj(file.file, f)
+
+    # Fetch registered dog list
     registered_dogs_list = get_registered_dogs()
+
+    # If registered dog list not empty 
     if registered_dogs_list:
+
         # Create an empty dictionary to store the retrieved data
         retrieved_dataset = {}
+
         # Iterate over the rows and populate the retrieved_dataset dictionary
         for registered_dog in registered_dogs_list:
             entry_id = registered_dog[0]
             feature_array_buffer = registered_dog[4]
             feature_array = np.frombuffer(feature_array_buffer, dtype=np.float32)
             retrieved_dataset[entry_id] = feature_array
+
         # Extract features from the new image
         user_inputed_image_features = extract_features(image_file_path)
+
         # Compare the new image features with the dataset using cosine similarity
         max_similarity = -1
         max_similarity_entry_id = None
+
         for entry_id, existing_img_features in retrieved_dataset.items():
             similarity = 1 - cosine(existing_img_features, user_inputed_image_features)
             if similarity > max_similarity:
                 max_similarity = similarity
                 max_similarity_entry_id = entry_id
+
+        # Convert number to one decimal number
         truncated_number = float(str(max_similarity)[:3])
+
         if truncated_number > 0.8:
-            return get_registered_dog_and_owner(max_similarity_entry_id)
+
+            results = get_registered_dog_and_owner(max_similarity_entry_id)
+            
+            if results:
+                return results
+            else:
+                return HTTPException(status_code=404)
+                
         else:
-            raise HTTPException(status_code=404)
+            return HTTPException(status_code=404)
+        
+    else:
+        return HTTPException(status_code=404)
+    
+
+# Endpoint to send image the image
+@app.get("/registered_dog_image/{entry_id}")
+async def get_dog_image(entry_id: int):
+    
+    # Fetch image data from the database
+    image_data = get_dog_img_by_entry_id(entry_id)
+    
+    if image_data:
+        
+        # Send image as a streaming response
+         return StreamingResponse(io.BytesIO(image_data), media_type="image/jpeg")
     else:
         return HTTPException(status_code=404)
     
